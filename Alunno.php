151b5239140d238301aad5ae372103631cfab710
lai2-24 <lai2-24@itismeucci.com>
2025-02-19 09:09:21 +0100

    arraySerializable

--- Alunno.php
+++ Alunno.php
@@ -7,14 +7,14 @@ class Alunno implements JsonSerializable{
     protected $cognome;
     protected $eta;
     protected $indirizzo;
-    protected $voto;
+    protected $voti;
 
-    function __construct($nome, $cognome, $eta, $via, $nCivico, $numero, $descrizione) {
+    function __construct($nome, $cognome, $eta, $via, $nCivico) {
         $this->nome = $nome;
         $this->cognome = $cognome;
         $this->eta = $eta;
         $this->indirizzo = new Indirizzo($via, $nCivico);
-        $this->voto = new Voto($numero, $descrizione);
+        $this->voti = array();
     }
 
     public function jsonSerialize():array{
@@ -23,8 +23,12 @@ class Alunno implements JsonSerializable{
             'cognome' => $this->cognome,
             'eta' => $this->eta,
             'indirizzo' => $this->indirizzo,
-            'voto' => $this->voto
+            'voti' => $this->voti
         ];
+    }
+
+    public function addVoto($numero, $descrizione){
+        array_push($this->voti, new Voto($numero, $descrizione));
     }
 
     function set_nome($nome) {
--- index.php
+++ index.php
@@ -5,9 +5,17 @@ header("Content-Type: application/json");
 
     $Alunni = array();
 
-    $Alunni[0] = new Alunno("Dario","Cannavacciuolo", 18, "Via Reggepi Frassinetti", 6, 10 ,"Bravo");
-    $Alunni[1] = new Alunno("Mattia","Iacob", 18, "Via Charles Baudleare", 69, 6 ,"Potevi fare di più");
-    $Alunni[2] = new Alunno("Isacco","Pieri", 19, "Via Lucio Corsi", 10, 4 ,"Hai fatto proprio schifo");
+    $Alunni[0] = new Alunno("Dario","Cannavacciuolo", 18, "Via Reggepi Frassinetti", 6);
+    $Alunni[1] = new Alunno("Mattia","Iacob", 18, "Via Charles Baudleare", 69);
+    $Alunni[2] = new Alunno("Isacco","Pieri", 19, "Via Lucio Corsi", 10, 4);
+
+    $Alunni[0]->addVoto(10, "very good");
+    $Alunni[0]->addVoto(2, "not good");
+
+    $Alunni[1]->addVoto(6, "normie good");
+
+    $Alunni[2]->addVoto(-1, "superBad good");
+
 
     echo json_encode($Alunni);
     
